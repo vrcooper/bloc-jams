689ef967f2aa3c9db1019f648716a037d3c956ec
Virginia <vcooper_us@yahoo.com>
2016-03-25 23:19:12 -0700

    Successfully refactored landing.js.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,16 @@
 var animatePoints = function() {
-  
-    var points = getElementByClassName('point');
-    
-   
-    
-    for(var i=0; i < points.length; i ++) 
-    
-    var revealPoint = function (i) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].stylemsTransform = "scaleX(1) translateY(0)";
-    points[i].styleWebkitTransform = "scale(1) translateY(0)";
-    };
-        revealPoint();
-   };  
-      
 
+    var points = document.getElementsByClassName('point');
+
+    var revealPoint = function (index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].stylemsTransform = "scaleX(1) translateY(0)";
+    points[index].styleWebkitTransform = "scale(1) translateY(0)";
+    };
+      
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    };
+}
 animatePoints();
