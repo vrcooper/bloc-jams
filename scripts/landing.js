312583e34355e66d542dded2dfe56d0f9d1e9714
Virginia <vcooper_us@yahoo.com>
2016-03-26 21:24:08 -0700

    Successfully added event handler.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,7 @@
-var animatePoints = function() {
-
-    var points = document.getElementsByClassName('point');
-
-    var revealPoint = function (index) {
+    var pointsArray = document.getElementsByClassName('point');
+    
+    var animatePoints = function(points) {
+        var revealPoint = function (index) {
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
     points[index].stylemsTransform = "scaleX(1) translateY(0)";
@@ -12,5 +11,19 @@ var animatePoints = function() {
     for (var i = 0; i < points.length; i++) {
         revealPoint(i);
     };
+};
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener("scroll", function(event) {
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+       }
+    });
 }
-animatePoints();
