3f7d5a8213b52182bf96371d934c8ae0005dfaa1
Virginia <vcooper_us@yahoo.com>
2016-04-14 22:03:43 -0700

    Refactored collection and album views with jQuery.

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+// # 1
+var buildCollectionItemTemplate = function() {
+    var template =
         '<div class="collection-album-container column fourth">'
         + '<img src="assets/images/album_covers/01.png"/>'
         + '<div class="collection-album-info caption">'
@@ -13,14 +15,22 @@ var collectionItemTemplate =
         + ' </div> '
         + ' </div> '
         ;
+    
+    // #2
+        return $(template);
+    };
 
-    window.onload = function() {
+    $(window).load(function() {
+         // # 3   
+        var $collectionContainer = $('.album-covers');
             
-        var collectionContainer = document.getElementsByClassName('album-covers')[0];
-            
-            collectionContainer.innerHTML = '';
+           // #4
+        $collectionContainer.empty();
             
             for (var i = 0; i < 12; i++)  {
-                collectionContainer.innerHTML += collectionItemTemplate;
+                var $newThumbnail = buildCollectionItemTemplate();
+                // #5
+                $collectionContainer.append($newThumbnail);
                  }
-            }
+            });
+                
